ea2563486bc1932e4599d10a6d6056fd9ffdb507
Meisuleen Kased <meisuleen@gmail.com>
2018-11-18 01:26:38 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -21,8 +21,6 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	programmer_hash[:alan_kay => {:known_for}]
-    puts programmer_hash
 
 	programmer_hash =
  		{
@@ -39,6 +37,10 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+programmer_hash[:alan_kay => {:known_for}]
+return programmer_hash
+     
 end
 
 def dennis_ritchies_language
